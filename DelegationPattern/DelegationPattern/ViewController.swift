892790ccfe952366b726e3f6f6c2e4a1c3302532
Hager Elsayed <Hasanieenh@mobilenoc.mobi>
2023-01-05 20:39:55 +0200

    add resources

--- DelegationPattern/DelegationPattern/ViewController.swift
+++ DelegationPattern/DelegationPattern/ViewController.swift
@@ -7,13 +7,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+public class LanguagesListViewController: UIViewController {
+    
+    lazy var tableView: UITableView = {
+        let tableview = UITableView(frame: .zero, style: .plain)
+        tableview.translatesAutoresizingMaskIntoConstraints = false
+        tableview.separatorStyle = .none
+        tableview.backgroundColor = .clear
+        tableview.dataSource = self
+        tableview.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return tableview
+    }()
+    
+    private let items = ["Swift", "React Native", "Flutter"]
+    
+    public override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setConstraints()
+        self.view.backgroundColor = .white
     }
-
-
 }
 
+// MARK: Helper
+extension LanguagesListViewController {
+    func setConstraints() {
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -5),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8)
+        ])
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension LanguagesListViewController: UITableViewDataSource {
+    public func tableView(_ tableView: UITableView,
+                          cellForRowAt indexPath: IndexPath)
+    -> UITableViewCell {
+        let cell =
+        tableView.dequeueReusableCell(withIdentifier: "cell",
+                                      for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell }
+    public func tableView(_ tableView: UITableView,
+                          numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+}
